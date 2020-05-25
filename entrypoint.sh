c8d367f0ec7b0c8bf12e7d90a6c5255e96640dd3
yellowmegaman <yellowmegaman@gmail.com>
2020-05-25 18:17:54 +0300

    kubeconfig output

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,42 +10,42 @@ docker cp k3s:/etc/rancher/k3s/k3s.yaml .
 
 sed -i "s/127.0.0.1/$INPUT_RUNNER_HOSTNAME/g" k3s.yaml
 
-#echo 'wait for k3s'
-#for attempt in {1..60}; do
-#	if kubectl version; then
-#		exit;
-#	else
-#		echo "k3s is not yet up"
-#		sleep 3
-#	fi
-#done
+echo 'wait for k3s'
+for attempt in {1..60}; do
+	if kubectl version; then
+		exit;
+	else
+		echo "k3s is not yet up"
+		sleep 3
+	fi
+done
 
-#echo 'wait for traefik is READY'
-#for attempt in {1..60}; do
-#	if kubectl -n kube-system get pod -o custom-columns=POD:metadata.name,READY:status.containerStatuses[*].ready | grep true | grep '^traefik'; then
-#		exit
-#	elif [ "$attempt" -eq 60 ]; then
-#		exit 1
-#	else
-#		sleep 1; echo -n '.'
-#	fi
-#done
-#
-#echo 'wait for coredns is READY'
+echo 'wait for traefik is READY'
+for attempt in {1..60}; do
+	if kubectl -n kube-system get pod -o custom-columns=POD:metadata.name,READY:status.containerStatuses[*].ready | grep true | grep '^traefik'; then
+		exit
+	elif [ "$attempt" -eq 60 ]; then
+		exit 1
+	else
+		sleep 1; echo -n '.'
+	fi
+done
 
-#for attempt in {1..60}; do
-#	if kubectl -n kube-system get pod -o custom-columns=POD:metadata.name,READY:status.containerStatuses[*].ready | grep true | grep '^coredns'; then
-#		exit
-#	elif [ "$attempt" -eq 60 ]; then
-#		exit 1
-#	else
-#		sleep 1
-#		echo -n '.'
-#	fi
-#done
+echo 'wait for coredns is READY'
+
+for attempt in {1..60}; do
+	if kubectl -n kube-system get pod -o custom-columns=POD:metadata.name,READY:status.containerStatuses[*].ready | grep true | grep '^coredns'; then
+		exit
+	elif [ "$attempt" -eq 60 ]; then
+		exit 1
+	else
+		sleep 1
+		echo -n '.'
+	fi
+done
 
 
-#echo 'get all resources'
-#kubectl get all --all-namespaces
+echo 'get all resources'
+kubectl get all --all-namespaces
 
 echo "::set-output name=kubeconfig::$(cat k3s.yaml)"
