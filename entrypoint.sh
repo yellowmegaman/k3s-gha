6db442bb7ecebe79f3623a71b3995c8349b42c78
yellowmegaman <yellowmegaman@gmail.com>
2020-05-25 17:05:41 +0300

    pwd

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,19 @@ export KUBECONFIG=k3s.yaml
 cp /registries.yaml .
 touch k3s.yaml
 
+echo '##############'
+pwd
+echo '##############'
+
 ls
+
 du -sh registries.yaml
 
 echo '#################'
 
-docker run -d --name=k3s --privileged --tmpfs /run --tmpfs /var/run -p 6443:6443 -p 80:80 -v "$PWD"/k3s.yaml:/etc/rancher/k3s/k3s.yaml -v "$PWD"/registries.yaml:/etc/rancher/k3s/registries.yaml rancher/k3s:$INPUT_K3S_TAG server
+#docker run -d --name=k3s --privileged --tmpfs /run --tmpfs /var/run -p 6443:6443 -p 80:80 -v "$PWD"/k3s.yaml:/etc/rancher/k3s/k3s.yaml -v "$PWD"/registries.yaml:/etc/rancher/k3s/registries.yaml rancher/k3s:$INPUT_K3S_TAG server
+
+docker run -d --name=k3s --privileged --tmpfs /run --tmpfs /var/run -p 6443:6443 -p 80:80 -v "$PWD"/k3s.yaml:/etc/rancher/k3s/k3s.yaml --mount 'type=bind,src=/home/yellowmegaman/za2dp.sh,dst=/eba.sh'
 
 sleep 30
 
